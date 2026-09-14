ea49f6a33b1fabf94b31759996756490b2e6036c
Kalp Dalsania <124522355+Itzklp@users.noreply.github.com>
2026-09-14 18:55:13 +0530

    Add: arranging-coins - submission-2

---
+++ Data Structures & Algorithms/arranging-coins/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if (n <= 3) {
+            return n == 1 ? 1 : n - 1;
+        }
+
+        int l = 1, r = (n / 2) + 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            long long coins = (mid * (mid + 1LL)) / 2;
+            if (coins <= n) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l - 1;
+    }
+};
